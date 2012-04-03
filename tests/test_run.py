bdee06dab6b021c822fff005efb521b8848d3ed3
Mike Plavsky <Mike.Plavsky@quest.com>
2012-04-03 18:07:53 +0000

    bulk run

---
+++ tests/test_run.py
@@ -0,0 +1,31 @@
+ip = "107.21.11.137"
+
+from nose.tools import *
+
+def run(name):
+
+  import urllib
+  test = urllib.urlopen("http://%s/%s?test"%(ip,name)).read()
+
+  import re
+  res = re.search( ", (\d*) wrong", test )
+
+  return res.groups()
+
+def check_res( name ):
+  
+  res = run( name )
+  eq_(res[0], 0)
+
+def test_run():
+
+  tests = [ 
+     "LabTests.RunAgents", 
+     "LabTests.MountSqlDb", 
+     "LabTests.AdFix.CreateUsers",
+     "LabTests.InfoPortal.InstallIp",
+  ] 
+
+  for t in tests:
+    yield check_res, t
+    
